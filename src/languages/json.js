490482024651802f7c5cd104f48502aee064c9fd
Fielding Johnston <fielding@justfielding.com>
2012-04-19 10:12:11 -0500

    added support for 'json' language; added small css editions to default.css, github.css, monokai.css to help prettyify the display

---
+++ src/languages/json.js
@@ -0,0 +1,26 @@
+/*
+Language: JSON
+Author: Fielding Johnston <fielding@justfielding.com>
+Website: justfielding.com
+Description: JSON highlighting for highlight.js
+*/
+
+hljs.LANGUAGES.json = function() {
+  return {
+    defaultMode: {
+      keywords: {
+        "literal": {'true': 5, 'false': 5, 'null': 5}
+      },
+      contains: [
+        hljs.C_NUMBER_MODE,
+        {
+          className: 'keyword',
+          begin: '(("|\')+([A-Za-z])+("|\')+):',
+          relevance: 0
+        },
+        hljs.APOS_STRING_MODE,
+        hljs.QUOTE_STRING_MODE
+      ]
+    }
+  };
+}();
